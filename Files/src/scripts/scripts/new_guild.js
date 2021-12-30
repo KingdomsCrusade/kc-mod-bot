3adb3229e02fe03b95fea9d13c97d7d8179821f0
Zac <zac91zac@gmail.com>
2021-12-30 18:20:23 +0000

    Create new_guild.js

---
+++ Files/src/scripts/scripts/new_guild.js
@@ -0,0 +1,7 @@
+const { Client, Channel } = require ('discord.js')
+const Discord = require('discord.js')
+const client = new Client()
+
+client.on('guildCreate', guild => {
+    guild.systemChannel.send('Thanks for adding me! This message will be updated in the future.').catch(x => x.return);
+  })
